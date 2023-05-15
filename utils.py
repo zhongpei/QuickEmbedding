7f3d540e041e76f19a92e9026124509118fdc526
ethansmith2000 <ethansmith2000@gmail.com>
2023-05-14 23:09:17 -0400

    readme + requirements

--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ else:
         "nearest": PIL.Image.NEAREST,
     }
 
-def load_embeds(pipe, embeddings_dir, SD_version="v1"):
+def load_embeds(pipe, embeddings_dir):
     paths = os.listdir(embeddings_dir)
     paths = [p for p in paths if p.endswith(".pt")] #TODO depends on what we use
 
@@ -44,27 +44,18 @@ def load_embeds(pipe, embeddings_dir, SD_version="v1"):
     for path in paths:
         embed_dict = torch.load(os.path.join(embeddings_dir,path))
 
-        if 'SD_version' in embed_dict.keys():
-            embed_version = embed_dict["SD_version"]
-        else:
-            # lazy fix
-            embed_version = SD_version
+        num_added_tokens += pipe.tokenizer.add_tokens(embed_dict["placeholder_token"])
+        placeholder_token_id = pipe.tokenizer.convert_tokens_to_ids(embed_dict["placeholder_token"])
 
-        if SD_version == embed_version:
-            num_added_tokens += pipe.tokenizer.add_tokens(embed_dict["placeholder_token"])
-            placeholder_token_id = pipe.tokenizer.convert_tokens_to_ids(embed_dict["placeholder_token"])
+        # Resize the token embeddings as we are adding new special tokens to the tokenizer
+        pipe.text_encoder.resize_token_embeddings(len(pipe.tokenizer))
 
-            # Resize the token embeddings as we are adding new special tokens to the tokenizer
-            pipe.text_encoder.resize_token_embeddings(len(pipe.tokenizer))
+        # load the embed
+        token_embeds = pipe.text_encoder.get_input_embeddings().weight.data
+        token_embeds[placeholder_token_id] = embed_dict["embedding"]
 
-            # load the embed
-            token_embeds = pipe.text_encoder.get_input_embeddings().weight.data
-            token_embeds[placeholder_token_id] = embed_dict["embedding"]
-
-            # running total of tokens to know what got loaded
-            loaded_tokens.append(embed_dict["placeholder_token"])
-        else:
-            print("token:" + embed_dict["placeholder_token"] + "not compatible with current SD version, skipping...")
+        # running total of tokens to know what got loaded
+        loaded_tokens.append(embed_dict["placeholder_token"])
 
     print("trained tokens added:", loaded_tokens)
 
